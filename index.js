b1a7cf6f192676620c6c6a57657c23609c10daa2
edgar-sb <edgar@space.bar>
2021-11-12 14:37:05 -0600

    Listo

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ conection.connect()
 
 xlsxFile('./dos.xlsx').then((r) => {
     r.map((c) => {
-        conection.query(`INSERT INTO Corporates (name, english_name, address,  ext_num, int_num, postal_code, colony, municipality, state, cel_code , cel_lada, cel, social_type, membership, created_at, updated_at) 
-                        values ('${c[0]}', '${c[1]}', '${c[2]}', '${c[3]}', '${c[4]}', '${c[5]}', '${c[6]}','${c[7]}', '${c[8]}', '${c[9]}', '${c[10]}', '${c[11]}', '${c[12]}', '${c[13]}',  '2021-11-12 18:00:39.107656000 +0000','2021-11-12 18:00:39.107656000 +0000') RETURNING *`)
+        conection.query(`INSERT INTO Corporates 
+        (name, english_name, address,  ext_num, int_num, postal_code, colony, municipality, state, cel_code , cel_lada, cel, social_type, membership, created_at, updated_at) values 
+        ('${c[0]}', '${c[1]}', '${c[2]}', '${c[3]}', '${c[4]}', '${c[5]}', '${c[6]}','${c[7]}', '${c[8]}', '${c[9]}', '${c[10]}', '${c[11]}', '${c[20]}', '${c[21]}',  '2021-11-12 18:00:39.107656000 +0000','2021-11-12 18:00:39.107656000 +0000') RETURNING *`)
             .then(res => {
                 //console.log(res.rows[0].id);
             }).catch(err => {
